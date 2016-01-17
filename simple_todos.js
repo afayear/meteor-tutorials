3bfd5b1fd04da47ba871b04ac664f82c68b64491
oneweekonething <oneweekonething@163.com>
2016-01-17 16:41:04 +0800

    refs #, Collections and forms and events

--- simple_todos.js
+++ simple_todos.js
@@ -1,14 +1,31 @@
+Tasks = new Mongo.Collection("tasks");
+
 if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault('counter', 0);
 
   Template.body.helpers({
-    tasks:[
-      {text: "This is task 1"},
-      {text: "This is task 2"},
-      {text: "This is task 3"}
-    ]
+    tasks: function(){
+      return Tasks.find({},{sort:{createdAt:-1}});
+    }
   });
+
+  Template.body.events({
+    "submit .new-task":function (event){
+      event.preventDefault();
+
+      var text = event.target.text.value;
+
+      Tasks.insert({
+        text: text,
+        createdAt: new Date()
+      })
+
+      event.target.text.value = "";
+    }
+  })
+
+
 }
 
 if (Meteor.isServer) {
