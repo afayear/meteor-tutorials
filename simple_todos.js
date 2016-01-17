dc356db69e3d554c22800fdd5bfe0da50919a8b8
oneweekonething <oneweekonething@163.com>
2016-01-17 17:25:41 +0800

    refs #3, Storing temporary UI state in Session

--- simple_todos.js
+++ simple_todos.js
@@ -6,8 +6,19 @@ if (Meteor.isClient) {
 
   Template.body.helpers({
     tasks: function(){
-      return Tasks.find({},{sort:{createdAt:-1}});
+      if (Session.get("hideCompleted")) {
+        return Tasks.find({checked: {$ne:true}},{sort:{createdAt: -1}});
+      }else{
+        return Tasks.find({},{sort:{createdAt:-1}});
+      }
+    },
+    hideCompleted: function(){
+      return Session.get("hideCompleted");
+    },
+    incompleteCount:function(){
+      return Tasks.find({checked: {$ne: true}}).count();
     }
+
   });
 
   Template.body.events({
@@ -22,6 +33,9 @@ if (Meteor.isClient) {
       })
 
       event.target.text.value = "";
+    },
+     "change .hide-completed input": function (event) {
+      Session.set("hideCompleted", event.target.checked);
     }
   })
 
@@ -35,7 +49,6 @@ if (Meteor.isClient) {
       Tasks.remove(this._id);
     }
   })
-
 }
 
 if (Meteor.isServer) {
